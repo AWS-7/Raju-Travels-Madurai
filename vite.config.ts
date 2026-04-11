75f6ad4d6d317f83896157cdec70ec685834d779
AWS-TEAM <agniwebsolution@gmail.com>
2026-04-11 17:20:09 +0530

    files upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Raju-Travels-Madurai/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
