ffc93b61a8bd2184c48e1ffa700688b413655080
AWS-TEAM <agniwebsolution@gmail.com>
2026-06-10 11:27:38 +0530

    files upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Raju-Travels-Madurai/',
+  base: '/',
   build: {
     // Enable code splitting
     rollupOptions: {
